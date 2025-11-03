5122c856ca36a54dc8811a9f4a99674b20f3d1d5
Juanda1211 <juandayclau1211@hotmail.com>
2025-11-03 09:34:07 -0500

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import tailwind from '@astrojs/tailwind';
 
 // Use the official Astro Tailwind integration instead of @tailwindcss/vite
 export default defineConfig({
+  site: 'https://juanda1211.github.io',
+  base: '/my-portfolio',
   integrations: [tailwind()],
 });
